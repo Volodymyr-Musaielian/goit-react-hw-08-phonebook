10f421a0b121f5efe487a04c0c558cbedcb94d33
Volodymyr-Musaielian <vovajahn@gmail.com>
2023-10-23 14:57:32 +0000

    Deploying to gh-pages from @ Volodymyr-Musaielian/goit-react-hw-08-phonebook@b4d1048d48996bfac20be9a18df747807b69a8da 🚀

---
+++ static/js/256.b8a87cf0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(t,e,r){r.r(e);r(791);var a=r(184),n={alineItems:"center",paddingTop:"15px"};e.default=function(){return(0,a.jsx)("img",{width:"500",src:"https://http.cat/404",alt:"Error 404. Not Found",style:n})}}}]);
+//# sourceMappingURL=256.b8a87cf0.chunk.js.map
